6b80c9197a718759ae33761081e52ec229688fa7
Ryuyeonjoo <fuswn87@gmail.com>
2022-11-04 15:28:56 +0900

    commit

--- src/Step1/Main.java
+++ src/Step1/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World! dff");  
     }
+
+    public int sum(int input) {
+        return 0;
+    }
 }
---
+++ src/Step3/Main.java
@@ -0,0 +1,32 @@
+package Step3;
+
+import java.util.Scanner;
+
+// backjoon8393_analysis
+public class Main {
+    // - 테스트 케이스
+    // 3 --> 6
+    // 5 --> 15
+public int sum(int input) {
+    int sum = 0;
+    for(int i=1; i <= input ; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+    public static void main(String[] args) {
+        // 반복값 입력
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+
+        // 합 구함.
+       Main main = new Main();
+       int output = main.sum(input);
+
+        // 합한 값 출력   
+        System.out.println(output);  
+    }
+
+}
